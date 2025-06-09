dc2786f675583adeb54aeb4df9adc44996e4afa1
Jayden <Jsterr08@Gmail.com>
2025-06-08 22:47:27 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    analytics: true,
-    edgeMiddleware: false
-  }),
-  integrations: [],
-  vite: {
-    optimizeDeps: {
-      exclude: ['firebase']
-    }
-  }
+    functionPerRoute: false,
+    edgeMiddleware: false,
+    imageService: true,
+    devImageService: 'sharp'
+  })
 });
